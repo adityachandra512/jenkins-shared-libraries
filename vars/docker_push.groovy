8c7459ec3851461274eb2a6b7aa8a3bccba7d280
adityachandra512 <143398217+adityachandra512@users.noreply.github.com>
2025-09-16 20:24:42 +0530

    Fix variable casing for Docker credentials

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String ImageTag) {
     withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-        sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+        sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
     }
 }
