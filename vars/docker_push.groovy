c58e659b6e27e2fbb3ace042f371eca39d27631e
adityachandra512 <143398217+adityachandra512@users.noreply.github.com>
2025-09-16 20:17:12 +0530

    Change docker login command syntax

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String ImageTag) {
     withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-        sh "echo ${dockerHubPass} | docker login -u ${dockerHubUser} --password-stdin"
+       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
         sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
     }
 }
