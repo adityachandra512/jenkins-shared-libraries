09a7f068c81e28818c25b80285dd157017ebb62d
adityachandra512 <143398217+adityachandra512@users.noreply.github.com>
2025-09-16 20:26:42 +0530

    Refactor docker_push function to accept DockerHub user

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,10 @@
-def call(String Project, String ImageTag) {
-    withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-        sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+def call(String Project, String ImageTag, String DockerHubUser) {
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhubcred',
+        usernameVariable: 'dockerHubUser',
+        passwordVariable: 'dockerHubPass'
+    )]) {
+        sh "echo ${dockerHubPass} | docker login -u ${dockerHubUser} --password-stdin"
+        sh "docker push ${DockerHubUser}/${Project}:${ImageTag}"
     }
 }
